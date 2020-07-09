555fa4e73c059dcda61f5d1df5f3550dd0d0e557
Iroha71 <y.koba.h1071@outlook.jp>
2020-07-09 23:04:32 +0900

    🎉first commit

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\develop\vradio\vradio_back\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ vradio_api/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class VradioApiConfig(AppConfig):
+    name = 'vradio_api'
